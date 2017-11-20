1c6aa698093c39c5d94d0eb5a57ada1dc49e2084
Antonio Vanegas <hpsaturn@gmail.com>
2017-11-20 00:36:27 -0500

    refactor native library for tests on Raspberry Pi 3

--- libframeprocess/src/main/java/com/admobilize/lib/NativeProcess.java
+++ libframeprocess/src/main/java/com/admobilize/lib/NativeProcess.java
@@ -9,7 +9,7 @@ public class NativeProcess {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("frameprocess");
     }
 
     /**
